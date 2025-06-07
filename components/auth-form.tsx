cc53d5005a0599f75335c2b542ede82e4139ee89
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-06-07 14:00:58 -0500

    sign in and sign up done with storing user data in db

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -100,12 +100,12 @@ const AuthForm = ({ type }: { type: FormType }) => {
   return (
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
-        <div className="flex flex-row gap-2 justify-center">
-          <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h2 className="text-primary-100">PrepWise</h2>
+        <div className="flex flex-col gap-2 justify-center items-center">
+          {/* <Image src="/logo.svg" alt="logo" height={32} width={38} /> */}
+          <h2 className="text-primary-100">Echo Mate</h2>
+          <h3 className="text-primary-200">AI Voice Executive Assistant</h3>
         </div>
 
-        <h3>Echo Mate : AI Voice Executive Assistant</h3>
 
         <Form {...form}>
           <form
